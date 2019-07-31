57dd8afed950a2c6c7e9254d94bef8514b53905b
Kochi <abcdcba123456789@gmail.com>
2019-07-31 18:56:55 +0900

    getintにて、存在しないエントリをロードすると例外となる問題を修正

--- FalconConfigParser.py
+++ FalconConfigParser.py
@@ -7,19 +7,27 @@
 
 
 import configparser
+import logging
+from logging import getLogger, FileHandler, Formatter
 
 
 class FalconConfigParser(configparser.ConfigParser):
 
 
-	fileName=""
+	def __init__(self):
+		super().__init__()
+		self.identifier="falconConfigParser"
+		self.log=getLogger("falcon.%s" % self.identifier)
+		self.log.debug("Create config instance")
 
 	def read(self,fileName):
 		self.fileName=fileName
+		self.log.info("read configFile:"+fileName)
 		return super().read(fileName)
 
 
 	def write(self):
+		self.log.info("write configFile:"+self.fileName)
 		with open(self.fileName,"w") as f: return super().write(f)
 
 	# 存在しないキーで読み出しを試行した場合、自動的にそのキーが生成される
@@ -27,16 +35,24 @@ class FalconConfigParser(configparser.ConfigParser):
 		try:
 			return FalconConfigSection(super().__getitem__(key))
 		except KeyError as e:
-			self.__setitem__(section,key,"")
-			return ""
+			self.log.debug("create new section:"+key)
+			self.add_section(key)
+			self.__getitem__(key)
 
 	#既に存在してもエラーにならないように変更
 	def getint(self,section,key,default=0):
 		try:
 			return super().getint(section,key)
 		except configparser.NoOptionError as e:
+			self.log.debug("add new intval "+str(default)+" at section "+section+", key "+key)
 			self[section][key]=str(default)
 			return int(default)
+		except configparser.NoSectionError as e:
+			self.log.debug("add new section and intval "+str(default)+" at section "+section+", key "+key)
+			self.add_section(section)
+			self.__getitem__(section).__setitem__(key,str(default))
+			return int(default)
+
 	def add_section(self,name):
 		if not self.has_section(name):
 			return super().add_section(name)
