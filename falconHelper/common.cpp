f44f49ffc1c56cfb5af35f52c1ddf967dbc87297
Yukio Nozawa <personal@nyanchangames.com>
2019-12-26 16:54:12 +0900

    Auto formatted C++ code

--- falconHelper/common.cpp
+++ falconHelper/common.cpp
@@ -4,24 +4,28 @@
 #include "defs.h"
 #include "ctlcolor.h"
 
-falcon_helper_funcdef void releasePtr(char *p){
-free(p);
+falcon_helper_funcdef void releasePtr(char *p)
+{
+    free(p);
 }
 
-falcon_helper_funcdef void copyMemory(void *dest, void *src, size_t sz){
-RtlCopyMemory(dest,src,sz);
+falcon_helper_funcdef void copyMemory(void *dest, void *src, size_t sz)
+{
+    RtlCopyMemory(dest, src, sz);
 }
 
-BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD  fdwReason, LPVOID lpReserved) {
-switch(fdwReason){
-case DLL_PROCESS_ATTACH:
-    CoInitialize(NULL);
-    initCtlcolor();
-break;
-case DLL_PROCESS_DETACH:
-CoUninitialize();
-freeCtlcolor();
-break;
-}
-	return TRUE;
+BOOL APIENTRY DllMain(HINSTANCE hInst, DWORD fdwReason, LPVOID lpReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        CoInitialize(NULL);
+        initCtlcolor();
+        break;
+    case DLL_PROCESS_DETACH:
+        CoUninitialize();
+        freeCtlcolor();
+        break;
+    }
+    return TRUE;
 }
--- falconHelper/disctype.cpp
+++ falconHelper/disctype.cpp
@@ -9,190 +9,214 @@
 #include <tchar.h>
 #include "defs.h"
 
-namespace enums{
-enum DISC_TYPE{
-BD_RW=0,
-BD_R,
-BD_ROM,
-HDDVD_RAM,
-HDDVD,
-HDDVD_ROM,
-DVD_PLUS_RW_DL,
-DVD_PLUS_R_DL,
-DVD_PLUS_R_DL_AND_RW,
-DVD_R_DL,
-DVD_R_DL_AND_RW,
-DVD_RAM,
-DVD_PLUS_RW,
-DVD_PLUS_R,
-DVD_RW,
-DVD_R,
-DVD_ROM,
-CD_RW,
-CD_R,
-CD_ROM,
-MO
+namespace enums
+{
+enum DISC_TYPE
+{
+    BD_RW = 0,
+    BD_R,
+    BD_ROM,
+    HDDVD_RAM,
+    HDDVD,
+    HDDVD_ROM,
+    DVD_PLUS_RW_DL,
+    DVD_PLUS_R_DL,
+    DVD_PLUS_R_DL_AND_RW,
+    DVD_R_DL,
+    DVD_R_DL_AND_RW,
+    DVD_RAM,
+    DVD_PLUS_RW,
+    DVD_PLUS_R,
+    DVD_RW,
+    DVD_R,
+    DVD_ROM,
+    CD_RW,
+    CD_R,
+    CD_ROM,
+    MO
 };
 }
 
-std::string _enum2str(int val){
-std::string ret="";
-switch(val){
-case enums::DISC_TYPE::BD_RW:
-ret="BD-RW";
-break;
-case enums::DISC_TYPE::BD_R:
-ret="BD-r";
-break;
-case enums::DISC_TYPE::BD_ROM:
-ret="BD-ROM";
-break;
-case enums::DISC_TYPE::HDDVD_RAM:
-ret="HDDVD-RAM";
-break;
-case enums::DISC_TYPE::HDDVD:
-ret="HDDVD";
-break;
-case enums::DISC_TYPE::HDDVD_ROM:
-ret="HDDVD-ROM";
-break;
-case enums::DISC_TYPE::DVD_PLUS_RW_DL:
-ret="DVD+RW DL";
-break;
-case enums::DISC_TYPE::DVD_PLUS_R_DL:
-ret="DVD+R DL";
-break;
-case enums::DISC_TYPE::DVD_PLUS_R_DL_AND_RW:
-ret="DVD+R DL/RW";
-break;
-case enums::DISC_TYPE::DVD_R_DL:
-ret="DVD-R DL";
-break;
-case enums::DISC_TYPE::DVD_R_DL_AND_RW:
-ret="DVD-R DL/RW";
-break;
-case enums::DISC_TYPE::DVD_RAM:
-ret="DVD-RAM";
-break;
-case enums::DISC_TYPE::DVD_PLUS_RW:
-ret="DVD+RW";
-break;
-case enums::DISC_TYPE::DVD_PLUS_R:
-ret="DVD+R";
-break;
-case enums::DISC_TYPE::DVD_RW:
-ret="DVD-RW";
-break;
-case enums::DISC_TYPE::DVD_R:
-ret="DVD-R";
-break;
-case enums::DISC_TYPE::DVD_ROM:
-ret="DVD-ROM";
-break;
-case enums::DISC_TYPE::CD_RW:
-ret="CD-RW";
-break;
-case enums::DISC_TYPE::CD_R:
-ret="CD-R";
-break;
-case enums::DISC_TYPE::CD_ROM:
-ret="CD-ROM";
-break;
-case enums::DISC_TYPE::MO:
-ret="MO";
-break;
-default:
-ret="UNKNOWN";
-}
-return ret;
-}
-
-
-int _isIn(std::vector<int> *v, int val){
-auto result = std::find((*v).begin(), (*v).end(), val);
-return result != (*v).end();
+std::string _enum2str(int val)
+{
+    std::string ret = "";
+    switch (val)
+    {
+    case enums::DISC_TYPE::BD_RW:
+        ret = "BD-RW";
+        break;
+    case enums::DISC_TYPE::BD_R:
+        ret = "BD-r";
+        break;
+    case enums::DISC_TYPE::BD_ROM:
+        ret = "BD-ROM";
+        break;
+    case enums::DISC_TYPE::HDDVD_RAM:
+        ret = "HDDVD-RAM";
+        break;
+    case enums::DISC_TYPE::HDDVD:
+        ret = "HDDVD";
+        break;
+    case enums::DISC_TYPE::HDDVD_ROM:
+        ret = "HDDVD-ROM";
+        break;
+    case enums::DISC_TYPE::DVD_PLUS_RW_DL:
+        ret = "DVD+RW DL";
+        break;
+    case enums::DISC_TYPE::DVD_PLUS_R_DL:
+        ret = "DVD+R DL";
+        break;
+    case enums::DISC_TYPE::DVD_PLUS_R_DL_AND_RW:
+        ret = "DVD+R DL/RW";
+        break;
+    case enums::DISC_TYPE::DVD_R_DL:
+        ret = "DVD-R DL";
+        break;
+    case enums::DISC_TYPE::DVD_R_DL_AND_RW:
+        ret = "DVD-R DL/RW";
+        break;
+    case enums::DISC_TYPE::DVD_RAM:
+        ret = "DVD-RAM";
+        break;
+    case enums::DISC_TYPE::DVD_PLUS_RW:
+        ret = "DVD+RW";
+        break;
+    case enums::DISC_TYPE::DVD_PLUS_R:
+        ret = "DVD+R";
+        break;
+    case enums::DISC_TYPE::DVD_RW:
+        ret = "DVD-RW";
+        break;
+    case enums::DISC_TYPE::DVD_R:
+        ret = "DVD-R";
+        break;
+    case enums::DISC_TYPE::DVD_ROM:
+        ret = "DVD-ROM";
+        break;
+    case enums::DISC_TYPE::CD_RW:
+        ret = "CD-RW";
+        break;
+    case enums::DISC_TYPE::CD_R:
+        ret = "CD-R";
+        break;
+    case enums::DISC_TYPE::CD_ROM:
+        ret = "CD-ROM";
+        break;
+    case enums::DISC_TYPE::MO:
+        ret = "MO";
+        break;
+    default:
+        ret = "UNKNOWN";
+    }
+    return ret;
 }
 
-int _getEjectability(std::vector<int> *val){
-return _isIn(val,IMAPI_PROFILE_TYPE_REMOVABLE_DISK);
+int _isIn(std::vector<int> *v, int val)
+{
+    auto result = std::find((*v).begin(), (*v).end(), val);
+    return result != (*v).end();
 }
 
-int _getDiscType(std::vector<int> *val){
-if(_isIn(val,IMAPI_PROFILE_TYPE_BD_REWRITABLE)){
-return enums::DISC_TYPE::BD_RW;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_BD_R_RANDOM_RECORDING) || _isIn(val,IMAPI_PROFILE_TYPE_BD_R_SEQUENTIAL)){
-return enums::DISC_TYPE::BD_R;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_BD_ROM)){
-return enums::DISC_TYPE::BD_ROM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_HD_DVD_RAM)){
-return enums::DISC_TYPE::HDDVD_RAM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_HD_DVD_RECORDABLE)){
-return enums::DISC_TYPE::HDDVD;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_HD_DVD_ROM)){
-return enums::DISC_TYPE::HDDVD_ROM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_RW_DUAL)){
-return enums::DISC_TYPE::DVD_PLUS_RW_DL;
+int _getEjectability(std::vector<int> *val)
+{
+    return _isIn(val, IMAPI_PROFILE_TYPE_REMOVABLE_DISK);
 }
 
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_R_DUAL) && _isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_RW)){
-return enums::DISC_TYPE::DVD_PLUS_R_DL_AND_RW;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_R_DUAL)){
-return enums::DISC_TYPE::DVD_PLUS_R_DL;
-}
-if(
-	(
-		_isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_SEQUENTIAL) || _isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_LAYER_JUMP)
-	) && (
-		_isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_REWRITABLE) || _isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_RW_SEQUENTIAL)
-	)
-){
-return enums::DISC_TYPE::DVD_R_DL_AND_RW;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_SEQUENTIAL) || _isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_LAYER_JUMP)){
-return enums::DISC_TYPE::DVD_R_DL;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_RAM)){
-return enums::DISC_TYPE::DVD_RAM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_RW)){
-return enums::DISC_TYPE::DVD_PLUS_RW;
+int _getDiscType(std::vector<int> *val)
+{
+    if (_isIn(val, IMAPI_PROFILE_TYPE_BD_REWRITABLE))
+    {
+        return enums::DISC_TYPE::BD_RW;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_BD_R_RANDOM_RECORDING) || _isIn(val, IMAPI_PROFILE_TYPE_BD_R_SEQUENTIAL))
+    {
+        return enums::DISC_TYPE::BD_R;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_BD_ROM))
+    {
+        return enums::DISC_TYPE::BD_ROM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_HD_DVD_RAM))
+    {
+        return enums::DISC_TYPE::HDDVD_RAM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_HD_DVD_RECORDABLE))
+    {
+        return enums::DISC_TYPE::HDDVD;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_HD_DVD_ROM))
+    {
+        return enums::DISC_TYPE::HDDVD_ROM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_RW_DUAL))
+    {
+        return enums::DISC_TYPE::DVD_PLUS_RW_DL;
+    }
+
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_R_DUAL) && _isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_RW))
+    {
+        return enums::DISC_TYPE::DVD_PLUS_R_DL_AND_RW;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_R_DUAL))
+    {
+        return enums::DISC_TYPE::DVD_PLUS_R_DL;
+    }
+    if (
+        (
+            _isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_SEQUENTIAL) || _isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_LAYER_JUMP)) &&
+        (_isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_REWRITABLE) || _isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_RW_SEQUENTIAL)))
+    {
+        return enums::DISC_TYPE::DVD_R_DL_AND_RW;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_SEQUENTIAL) || _isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_R_DUAL_LAYER_JUMP))
+    {
+        return enums::DISC_TYPE::DVD_R_DL;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_RAM))
+    {
+        return enums::DISC_TYPE::DVD_RAM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_RW))
+    {
+        return enums::DISC_TYPE::DVD_PLUS_RW;
+    }
+
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_PLUS_R))
+    {
+        return enums::DISC_TYPE::DVD_PLUS_R;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_REWRITABLE) || _isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_RW_SEQUENTIAL))
+    {
+        return enums::DISC_TYPE::DVD_RW;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVD_DASH_RECORDABLE))
+    {
+        return enums::DISC_TYPE::DVD_R;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_DVDROM))
+    {
+        return enums::DISC_TYPE::DVD_ROM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_CD_REWRITABLE))
+    {
+        return enums::DISC_TYPE::CD_RW;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_CD_RECORDABLE))
+    {
+        return enums::DISC_TYPE::CD_R;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_CDROM))
+    {
+        return enums::DISC_TYPE::CD_ROM;
+    }
+    if (_isIn(val, IMAPI_PROFILE_TYPE_MO_ERASABLE) || _isIn(val, IMAPI_PROFILE_TYPE_AS_MO) || _isIn(val, IMAPI_PROFILE_TYPE_AS_MO))
+    {
+        return enums::DISC_TYPE::MO;
+    }
+    return -1;
 }
 
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_PLUS_R)){
-return enums::DISC_TYPE::DVD_PLUS_R;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_REWRITABLE) || _isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_RW_SEQUENTIAL)){
-return enums::DISC_TYPE::DVD_RW;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVD_DASH_RECORDABLE)){
-return enums::DISC_TYPE::DVD_R;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_DVDROM)){
-return enums::DISC_TYPE::DVD_ROM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_CD_REWRITABLE)){
-return enums::DISC_TYPE::CD_RW;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_CD_RECORDABLE)){
-return enums::DISC_TYPE::CD_R;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_CDROM)){
-return enums::DISC_TYPE::CD_ROM;
-}
-if(_isIn(val,IMAPI_PROFILE_TYPE_MO_ERASABLE) || _isIn(val,IMAPI_PROFILE_TYPE_AS_MO) || _isIn(val,IMAPI_PROFILE_TYPE_AS_MO)){
-return enums::DISC_TYPE::MO;
-}
-return -1;
-}
-
-falcon_helper_funcdef char* getDiscDriveTypes()
+falcon_helper_funcdef char *getDiscDriveTypes()
 {
     IDiscMaster2 *lpDiscMaster = NULL;
     HRESULT rMaster = CoCreateInstance(CLSID_MsftDiscMaster2, NULL, CLSCTX_ALL, IID_PPV_ARGS(&lpDiscMaster));
@@ -250,12 +274,12 @@ falcon_helper_funcdef char* getDiscDriveTypes()
         std::vector<int> profile_values;
         for (int i = 0; i < profiles->rgsabound[0].cElements; i++)
         {
-            //Ç±ÇÍÉRÉÅÉìÉgÉAÉEÉgÇµÇΩÇÁÅAÉTÉ|Å[ÉgÇµÇƒÇ¢ÇÈÉvÉçÉtÉ@ÉCÉãÇÃàÍóóÇ™èoÇÈ
+            //ÔøΩÔøΩÔøΩÔøΩRÔøΩÔøΩÔøΩÔøΩÔøΩgÔøΩAÔøΩEÔøΩgÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩAÔøΩTÔøΩ|ÔøΩ[ÔøΩgÔøΩÔøΩÔøΩƒÇÔøΩÔøΩÔøΩvÔøΩÔøΩÔøΩtÔøΩ@ÔøΩCÔøΩÔøΩÔøΩÃàÍóóÔøΩÔøΩÔøΩoÔøΩÔøΩ
             //std::cout << "    profile " << tmp[i].lVal << std::endl;
             profile_values.push_back(tmp[i].lVal);
         }
-        int ejectable= _getEjectability(&profile_values);
-        int type= _getDiscType(&profile_values);
+        int ejectable = _getEjectability(&profile_values);
+        int type = _getDiscType(&profile_values);
         SafeArrayDestroy(profiles);
 
         s << path << "," << ejectable << "," << _enum2str(type) << std::endl;
@@ -265,10 +289,9 @@ falcon_helper_funcdef char* getDiscDriveTypes()
 
     lpDiscMaster->Release();
 
-    std::string ret_s=s.str();
-    char* ret=(char*)malloc(ret_s.size()+1);
-    memset(ret,0,ret_s.size()+1);
-    memcpy(ret,ret_s.c_str(),ret_s.size());
+    std::string ret_s = s.str();
+    char *ret = (char *)malloc(ret_s.size() + 1);
+    memset(ret, 0, ret_s.size() + 1);
+    memcpy(ret, ret_s.c_str(), ret_s.size());
     return ret;
 }
-
