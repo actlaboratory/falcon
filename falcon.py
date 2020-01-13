fc0dc2a138d7ebc72cdb35e97d8e0500ade7720b
Kochi <abcdcba123456789@gmail.com>
2020-01-13 21:31:31 +0900

    実行時のカレントディレクトリを問わずDLLがロードできるよう対策

--- falcon.py
+++ falcon.py
@@ -2,19 +2,24 @@
 #Falcon startup file
 #run python fal.py to execute Falcon
 #Copyright (C) 2019 Yukio Nozawa <personal@nyanchangames.com>
+#Copyright (C) 2020 yamahubuki <itiro.ishino@gmail.com>
 #See window.py for application entry point
 
 import win32timezone#ダミー
 
 def _(string): pass#dummy
 
-from app import *
+#dllを相対パスで指定した時のため、カレントディレクトリを変更
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+import app as application
 import constants
 import globalVars
 
 def main():
 	global app
-	app=falconAppMain()
+	app=application.falconAppMain()
 	globalVars.app=app
 	app.initialize()
 	app.MainLoop()
