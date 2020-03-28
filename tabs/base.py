ddb3f5493c387a9f5c7408682934a963dadf8d89
nyanchan <personal@nyanchangames.com>
2020-03-28 13:36:54 +0900

    Fix error

--- tabs/base.py
+++ tabs/base.py
@@ -99,7 +99,7 @@ class FalconTabBase(object):
 			self.environment["markedPlace"]=None		#マークフォルダ
 			self.environment["selectedItemCount"]=None	#選択中のアイテム数。0or1or2=2以上。
 			self.environment["selectingItemCount"]={}	#選択中アイテムの種類(browsableObjects)毎の個数
-			self.checkedItem=set()		self.stopSoundHandle=None
+			self.checkedItem=set()
 
 	def SetEnvironment(self,newEnv):
 		"""タブの引継ぎなどの際にenvironmentの内容をコピーするために利用"""
