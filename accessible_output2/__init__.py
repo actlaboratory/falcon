b783c5f453d4e17ab479e73b233ce6216435b4c8
Kochi <abcdcba123456789@gmail.com>
2020-09-22 17:25:58 +0900

    Python3.8最適化、platform_utils削除

--- accessible_output2/__init__.py
+++ accessible_output2/__init__.py
@@ -1,22 +1,24 @@
+#2020.09.22 Python3.8対応、platform_util除去対応実施 (yamahubuki)
+
+
 from __future__ import absolute_import
 import ctypes
 import os
 import sys
 import types
-from platform_utils import paths
 
-#Python3.8以降での実行時のみ、dllの探索対象ディレクトリの指定が必要
-if sys.version_info.major>=3 and sys.version_info.minor>=8:
-	if paths.is_frozen():
-		os.add_dll_directory(os.path.join(paths.embedded_data_path(), 'accessible_output2', 'lib'))
-	else:
-		os.add_dll_directory(os.path.join(paths.module_path(), 'lib'))
 
 def load_library(libname, cdll=False):
-	if paths.is_frozen():
-		libfile = os.path.join(paths.embedded_data_path(), 'accessible_output2', 'lib', libname)
+	if hasattr(sys,"frozen"):
+		if sys.version_info.major>=3 and sys.version_info.minor>=8:
+			os.add_dll_directory(os.path.join(os.path.abspath(os.path.dirname(sys.executable)), 'lib'))
+		libfile = os.path.join(os.path.abspath(os.path.dirname(sys.executable)), 'accessible_output2', 'lib', libname)
 	else:
-		libfile = os.path.join(paths.module_path(), 'lib', libname)
+		import inspect
+		module_path=os.path.abspath(os.path.dirname(inspect.getmodule(inspect.stack()[0][0]).__file__))
+		if sys.version_info.major>=3 and sys.version_info.minor>=8:
+			os.add_dll_directory(os.path.join(module_path, 'lib'))
+		libfile = os.path.join(module_path, 'lib', libname)
 	if cdll:
 		return ctypes.cdll[libfile]
 	else:
