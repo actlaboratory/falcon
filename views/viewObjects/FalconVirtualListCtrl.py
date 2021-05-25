193268f466fb328651389bc7f9a3f24214eabfa6
Kochi <abcdcba123456789@gmail.com>
2021-05-25 22:18:06 +0900

    メインビューのバーチャルリスト対応に伴い、メインビュー以外でリストビューを開くとエラーが発生していた不具合を修正

--- views/viewObjects/FalconVirtualListCtrl.py
+++ views/viewObjects/FalconVirtualListCtrl.py
@@ -18,12 +18,19 @@ class FalconVirtualListCtrl(virtualListCtrlBase.virtualListCtrl):
         self.lst[item].list_imageIndex = image
 
     def OnGetItemAttr(self, item):
-        ret = self.lst[item].list_backgroundColour
-        if ret:
-            self.CheckedItemAttr = wx.ItemAttr()
-            self.CheckedItemAttr.SetBackgroundColour(wx.Colour(ret))
-            return self.CheckedItemAttr
-        return None
+        ret = self.lst[item]
+        if type(ret) == list:
+            return None
+        else:
+            ret = ret.list_backgroundColour
+            if ret:
+                self.CheckedItemAttr = wx.ItemAttr()
+                self.CheckedItemAttr.SetBackgroundColour(wx.Colour(ret))
+                return self.CheckedItemAttr
+            return None
 
     def OnGetItemImage(self, item):
-        return self.lst[item].list_imageIndex
+        if type(self.lst[item]) == list:
+            return -1
+        else:
+            return self.lst[item].list_imageIndex
