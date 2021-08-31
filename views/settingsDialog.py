46e5e1b03ae2d021642dadc4fa7e5c4e12e1fbc2
Kazto Kitabatake <kitabatake1013@outlook.jp>
2021-08-31 19:01:07 +0900

    設定項目を追加

--- views/settingsDialog.py
+++ views/settingsDialog.py
@@ -19,9 +19,30 @@ class configType(Enum):
 
 
 class Dialog(BaseDialog):
+    readerSelection = {
+        "NOSPEECH": _("音声なし"),
+        "AUTO": _("自動選択"),
+        "SAPI5": "SAPI5",
+        "CLIPBOARD": _("クリップボード出力"),
+        "PCTK": "PC-Talker",
+        "NVDA": "NVDA",
+        "JAWS": "JAWS for Windows"
+    }
     colorModeSelection = {
         "white": _("標準"),
         "dark": _("ダーク")
+    }
+    logLevelSelection = {
+        "50": "CRITICAL",
+        "40": "ERROR",
+        "30": "WARNING",
+        "20": "INFO",
+        "10": "DEBUG",
+        "0": "NOTSET"
+    }
+    textWrappingSelection = {
+        "on": _("画面幅で折り返し"),
+        "off": _("折り返さない")
     }
     languageSelection = constants.SUPPORTING_LANGUAGE
 
@@ -44,6 +65,25 @@ class Dialog(BaseDialog):
         self.creator = views.ViewCreator.ViewCreator(self.viewMode, self.panel, self.sizer, wx.VERTICAL, 20)
         self.tab = self.creator.tabCtrl(_("カテゴリ選択"))
 
+        # general
+        creator = views.ViewCreator.ViewCreator(self.viewMode, self.tab, None, views.ViewCreator.GridBagSizer,
+                                                label=_("一般"), style=wx.ALL | wx.EXPAND, proportion=1, margin=20)
+        self.startpath, static = creator.inputbox(_("起動時に開くフォルダ"))
+        self.startpathBrowse = creator.button(_("参照"), self.browseDir,)
+        self.logLevel, dummy = creator.combobox(_("ログ記録レベル(&L)"), list(self.logLevelSelection.values()))
+        self.reader, static = creator.combobox(_("出力先(&O)"), list(self.readerSelection.values()))
+        self.fxvolumeSpin, static = creator.spinCtrl(_("効果音の音量"), 0, 300, self.spinChanged)
+        self.fxvolumeSlider, static = creator.slider(_("効果音の音量"), 0, 300, self.sliderChanged)
+        self.historycount, static = creator.spinCtrl(_("検索履歴の保持件数"), 0, 100)
+
+        # preview
+        creator = views.ViewCreator.ViewCreator(self.viewMode, self.tab, None, views.ViewCreator.GridBagSizer,
+                                                label=_("プレビュー"), style=wx.ALL | wx.EXPAND, proportion=1, margin=20)
+        self.headerlinecount, static = creator.spinCtrl(_("テキストヘッダー行数"), 1, 100)
+        self.footerlinecount, static = creator.spinCtrl(_("テキストフッター行数"), 1, 100)
+        self.audiovolumeSpin, static = creator.spinCtrl(_("プレビューの音量"), 0, 300, self.spinChanged)
+        self.audiovolumeSlider, static = creator.slider(_("プレビューの音量"), 0, 300, self.sliderChanged)
+
         # view
         creator = views.ViewCreator.ViewCreator(
             self.viewMode,
@@ -55,6 +95,7 @@ class Dialog(BaseDialog):
             margin=20)
         self.language, static = creator.combobox(_("言語(&L)"), list(self.languageSelection.values()))
         self.colormode, static = creator.combobox(_("画面表示モード(&D)"), list(self.colorModeSelection.values()))
+        self.textwrapping, static = creator.combobox(_("テキストの折り返し(&W)"), list(self.textWrappingSelection.values()))
 
         # network
         creator = views.ViewCreator.ViewCreator(self.viewMode, self.tab, None, wx.VERTICAL, space=20, label=_("ネットワーク"), style=wx.ALL, margin=20)
@@ -70,9 +111,23 @@ class Dialog(BaseDialog):
         self.cancelBtn = creator.cancelbutton(_("キャンセル"), proportion=1)
 
     def load(self):
+        # general
+        self._setValue(self.startpath, "browse", "startpath", configType.STRING)
+        self._setValue(self.reader, "speech", "reader", configType.DIC, self.readerSelection)
+        self._setValue(self.logLevel, "general", "log_level", configType.DIC, self.logLevelSelection)
+        self._setValue(self.fxvolumeSpin, "speech", "fx_volume", configType.INT, 100, 0, 300)
+        self.fxvolumeSlider.SetValue(self.fxvolumeSpin.GetValue())
+        self._setValue(self.historycount, "search", "history_count", configType.INT, 0, 0, 100)
+
+        # preview
+        self._setValue(self.headerlinecount, "preview", "header_line_count", configType.INT, 10, 1, 100)
+        self._setValue(self.footerlinecount, "preview", "footer_line_count", configType.INT, 10, 1, 100)
+        self._setValue(self.audiovolumeSpin, "preview", "audio_volume", configType.INT, 100, 0, 300)
+        self.audiovolumeSlider.SetValue(self.audiovolumeSpin.GetValue())
         # view
         self._setValue(self.language, "general", "language", configType.DIC, self.languageSelection)
         self._setValue(self.colormode, "view", "colormode", configType.DIC, self.colorModeSelection)
+        self._setValue(self.textwrapping, "view", "textwrapping", configType.DIC, self.textWrappingSelection)
 
         # network
         self._setValue(self.update, "general", "update", configType.BOOL)
@@ -90,7 +145,7 @@ class Dialog(BaseDialog):
         self.server.Enable(result)
         self.port.Enable(result)
 
-    def _setValue(self, obj, section, key, t, prm=None, prm2=None):
+    def _setValue(self, obj, section, key, t, prm=None, prm2=None, prm3=None):
         assert isinstance(obj, wx.Window)
         assert type(section) == str
         assert type(key) == str
@@ -112,6 +167,11 @@ class Dialog(BaseDialog):
                 prm = ""
             assert type(prm) == str
             obj.SetValue(conf.getstring(section, key, prm, prm2))
+        elif t == configType.INT:
+            assert type(prm) == int
+            assert type(prm2) == int
+            assert type(prm3) == int
+            obj.SetValue(conf.getint(section, key, prm, prm2, prm3))
         self.iniDic[obj] = (t, section, key, prm, prm2)
 
     def _save(self):
@@ -122,3 +182,27 @@ class Dialog(BaseDialog):
             else:
                 conf[v[1]][v[2]] = obj.GetValue()
         conf.write()
+
+    def browseDir(self, event):
+        target = self.startpath
+        dialog = wx.DirDialog(self.wnd, _("起動時に開くフォルダを選択"))
+        result = dialog.ShowModal()
+        if result == wx.ID_CANCEL:
+            return
+        target.SetValue(dialog.GetPath())
+
+    def spinChanged(self, event):
+        obj = event.GetEventObject()
+        if obj == self.fxvolumeSpin:
+            target = self.fxvolumeSlider
+        elif obj == self.audiovolumeSpin:
+            target = self.audiovolumeSlider
+        target.SetValue(obj.GetValue())
+
+    def sliderChanged(self, event):
+        obj = event.GetEventObject()
+        if obj == self.fxvolumeSlider:
+            target = self.fxvolumeSpin
+        elif obj == self.audiovolumeSlider:
+            target = self.audiovolumeSpin
+        target.SetValue(obj.GetValue())
